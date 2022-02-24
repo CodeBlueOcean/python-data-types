d38984c58c3e2354688ac1b62b620e59304edf6d
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-02-24 08:53:10 -0600

    Add files via upload

---
+++ python-data-types.py
@@ -0,0 +1,42 @@
+#Fundamental Data Types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized Data Types
+
+None
+
+#Fundamental Data Types
+# int and float
+
+# print(type(2 + 4))
+# print(type(2 - 4))
+# print(type(2 * 4))
+# print(type(2 / 4)) # 0.5 floating point number
+
+# print(type(5.00001))
+
+# print(type(0))
+
+# print(type(20+1.1)) # still a float
+
+# print(type(9.9+1.1))
+
+print(2 ** 3)
+print(5 // 4) # round to an integer
+print(6 % 4)
+
+# math function
+print(round(3.1))
+
+print(abs(-20))
+
+
